d4bb96fd6dfbc4ec1a761b740b50fec103c2b809
OKrawi <krawi.ossama@gmail.com>
2021-04-03 01:41:25 +0300

    End of Section 13

---
+++ API/Helpers/PaginationHeader.cs
@@ -0,0 +1,22 @@
+namespace API.Helpers
+{
+    public class PaginationHeader
+    {
+        public PaginationHeader(
+            int currentPage, 
+            int itemsPerPage, 
+            int totalItems, 
+            int totalPages)
+        {
+            CurrentPage = currentPage;
+            ItemsPerPage = itemsPerPage;
+            TotalPages = totalPages;
+            TotalItems = totalItems;
+        }
+
+        public int CurrentPage {get; set;}
+        public int ItemsPerPage {get; set;}
+        public int TotalPages {get; set;}
+        public int TotalItems {get; set;}
+    }
+}
